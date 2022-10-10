a477e7eeaa84b44be92c09193fafcf01312283f5
Mali Tian <mltian@thoughtworks.com>
2022-10-10 12:51:58 +0800

    Clean up the greeting

--- src/test/java/com/tw/Controllers/BaseControllerTest.java
+++ src/test/java/com/tw/Controllers/BaseControllerTest.java
@@ -1,4 +1,4 @@
-package com.tw;
+package com.tw.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
