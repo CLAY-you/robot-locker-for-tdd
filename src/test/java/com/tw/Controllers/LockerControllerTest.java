c727b21cbaa33243ffb42244fd1023a50e9837c3
Hongkui Zhang <hongkui.zhang@thoughtworks.com>
2023-02-25 19:46:55 +0800

    [refactor] reallocate the tests in controller layer and service layer

--- src/test/java/com/tw/Controllers/LockerControllerTest.java
+++ src/test/java/com/tw/Controllers/LockerControllerTest.java
@@ -2,6 +2,7 @@ package com.tw.Controllers;
 
 import com.tw.LockerStatus;
 import com.tw.Services.LockerService;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
@@ -21,110 +22,101 @@ public class LockerControllerTest extends BaseControllerTest {
     @MockBean
     LockerService lockerService;
 
-    /*** Test case: locker 有可用的 slot
-     设计：方法 lockerService.getLockerStatus(): LockerStatus {hasAvailableSlot: boolean}
-     **Given** lockerController收到请求，并通过lockerService获取locker的status
-     **When** 向 /locker发送get请求
-     **Then** get /locker response返回Object LockerStatus {hasAvailableSlot: true}
-     */
-    @Test
-    void should_return_hasAvailableSlot_as_true_given_locker_has_available_slot() throws Exception {
-        LockerStatus lockerStatus = new LockerStatus(Boolean.TRUE);
-        when(lockerService.getLockerStatus()).thenReturn(lockerStatus);
-        this.mockMvc.perform(get("/locker")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.hasAvailableSlot").value(true));
+    @Nested
+    class CheckingWhetherThereIsAvailableSlot extends BaseControllerTest {
+        @Test
+        void should_return_hasAvailableSlot_as_true_given_locker_has_available_slot() throws Exception {
+            LockerStatus lockerStatus = new LockerStatus(Boolean.TRUE);
+            when(lockerService.getLockerStatus()).thenReturn(lockerStatus);
+            this.mockMvc.perform(get("/locker")).andDo(print()).andExpect(status().isOk())
+                    .andExpect(jsonPath("$.hasAvailableSlot").value(true));
+        }
+
+        @Test
+        void should_return_hasAvailableSlot_as_false_given_locker_has_no_available_slot() throws Exception {
+            LockerStatus lockerStatus = new LockerStatus(Boolean.FALSE);
+            when(lockerService.getLockerStatus()).thenReturn(lockerStatus);
+            this.mockMvc.perform(get("/locker")).andDo(print()).andExpect(status().isOk())
+                    .andExpect(jsonPath("$.hasAvailableSlot").value(false));
+
+        }
+
+        @Test
+        void should_return_500_given_locker_service_is_not_available() throws Exception {
+            when(lockerService.getLockerStatus()).thenThrow(new RuntimeException());
+            this.mockMvc.perform(get("/locker"))
+                    .andExpect(status().isInternalServerError())
+                    .andExpect(status().reason("Internal Server Error"));
+        }
     }
 
-    /*** Test case: locker 没有可用的 slot
-     **Given** lockerController收到请求，并通过lockerService获取locker的status
-     **When** 向 /locker发送get请求
-     **Then** get /locker response返回Object LockerStatus {hasAvailableSlot: false}
-     */
-    @Test
-    void should_return_hasAvailableSlot_as_false_given_locker_has_no_available_slot() throws Exception {
-        LockerStatus lockerStatus = new LockerStatus(Boolean.FALSE);
-        when(lockerService.getLockerStatus()).thenReturn(lockerStatus);
-        this.mockMvc.perform(get("/locker")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.hasAvailableSlot").value(false));
+    @Nested
+    class GettingAvailableSlotToSavePackage extends BaseControllerTest {
+        //TODO: locker 中存在可使用的slot的时候，返回8位随机数字返回 作为ticket number
+        @Test
+        void should_return_ticket_number_when_get_available_slot() throws Exception {
+            String ticketNo = "12345678";
+            when(lockerService.getTicketNoBindWithDispatchedSlot()).thenReturn(ticketNo);
+            this.mockMvc.perform(get("/slot")).andDo(print()).andExpect(status().isOk())
+                    .andExpect(jsonPath("$").value(ticketNo));
+        }
 
+        //TODO: locker 中不存在可使用的slot的时候，返回提示信息 "储物柜已满，请稍后再试"
+        @Test
+        void should_return_warning_message_no_available_slot_when_not_find_available_slot() throws Exception {
+            String warningMessage = "no available slot can be dispatched, try it later";
+            when(lockerService.getTicketNoBindWithDispatchedSlot()).thenReturn(warningMessage);
+            this.mockMvc.perform(get("/slot")).andDo(print()).andExpect(status().isOk())
+                    .andExpect(jsonPath("$").value(warningMessage));
+        }
+
+        //TODO: locker 中有可用的箱位，系统报错时候，提示 "存包异常， 请稍后再试"
+        @Test
+        void should_return_warning_message_no_available_slot_when_not_internal_server_error_occurred() throws Exception {
+            String warningMessage = "Internal Server Error";
+            doThrow(new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, warningMessage))
+                    .when(lockerService).getTicketNoBindWithDispatchedSlot();
+            this.mockMvc.perform(get("/slot")).andDo(print())
+                    .andExpect(status().isInternalServerError())
+                    .andExpect(status().reason(warningMessage))
+                    .andExpect(result -> assertTrue(result.getResolvedException() instanceof ResponseStatusException));
+        }
     }
 
-    /*** Test case: locker 服务不可用
-     **Given** lockerController收到请求，并通过lockerService获取locker的status
-     **When** 向 /locker发送get请求
-     **Then** get /locker response返回  {status: 500, message: "Service not available"}
-     */
 
-    @Test
-    void should_return_500_given_locker_service_is_not_available() throws Exception {
-        when(lockerService.getLockerStatus()).thenThrow(new RuntimeException());
-        this.mockMvc.perform(get("/locker"))
-                .andExpect(status().isInternalServerError())
-                .andExpect(status().reason("Internal Server Error"));
-    }
-    //TODO: locker 中存在可使用的slot的时候，返回8位随机数字返回 作为ticket number
+    @Nested
+    class GettingOccupiedSlotBeforeToTakePackageBack extends BaseControllerTest {
+        //TODO: 通过locker已经存入一个包，通过拿到的 ticket no 取出存入的包裹，返回slot id
+        @Test
+        void should_return_saved_slot_info_when_received_dispatched_ticket_no_before() throws Exception {
+            String ticketNo = "12345678";
+            String slotId = "123";
+            when(lockerService.getSlotInfoByTicketNoDispatched(ticketNo)).thenReturn(slotId);
+            this.mockMvc.perform(get("/slot/12345678")).andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$").value("123"));
+        }
 
-    @Test
-    void should_return_ticket_number_when_get_available_slot() throws Exception {
-        String ticketNo = "12345678";
-        when(lockerService.getTicketNoBindWithDispatchedSlot()).thenReturn(ticketNo);
-        this.mockMvc.perform(get("/slot")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$").value(ticketNo));
-    }
+        //TODO: 通过locker已经存入一个包，第一次成功取包后，重复使用ticket no 取包，返回 warning message
+        @Test
+        void should_return_warning_message_ticket_number_is_invalid_getting_package_failed() throws Exception {
+            String ticketNo = "12345678";
+            String warningMessage = "ticket number is invalid";
+            when(lockerService.getSlotInfoByTicketNoDispatched(ticketNo)).thenReturn(warningMessage);
+            this.mockMvc.perform(get("/slot/12345678")).andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$").value(warningMessage));
+        }
 
-    //TODO: locker 中不存在可使用的slot的时候，返回提示信息 "储物柜已满，请稍后再试"
-
-    @Test
-    void should_return_warning_message_no_available_slot_when_not_find_available_slot() throws Exception {
-        String warningMessage = "no available slot can be dispatched, try it later";
-        when(lockerService.getTicketNoBindWithDispatchedSlot()).thenReturn(warningMessage);
-        this.mockMvc.perform(get("/slot")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$").value(warningMessage));
-    }
-
-    //TODO: locker 中有可用的箱位，系统报错时候，提示 "存包异常， 请稍后再试"
-
-    @Test
-    void should_return_warning_message_no_available_slot_when_not_internal_server_error_occurred() throws Exception {
-        String warningMessage = "Internal Server Error";
-        doThrow(new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, warningMessage))
-                .when(lockerService).getTicketNoBindWithDispatchedSlot();
-        this.mockMvc.perform(get("/slot")).andDo(print())
-                .andExpect(status().isInternalServerError())
-                .andExpect(status().reason(warningMessage))
-                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ResponseStatusException));
-    }
-
-    //TODO: 通过locker已经存入一个包，通过拿到的 ticket no 取出存入的包裹，返回slot id
-    @Test
-    void should_return_saved_slot_info_when_received_dispatched_ticket_no_before() throws Exception {
-        String ticketNo = "12345678";
-        String slotId = "123";
-        when(lockerService.getSlotInfoByTicketNoDispatched(ticketNo)).thenReturn(slotId);
-        this.mockMvc.perform(get("/slot/12345678")).andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").value("123"));
-    }
-
-    //TODO: 通过locker已经存入一个包，第一次成功取包后，重复使用ticket no 取包，返回 warning message
-    @Test
-    void should_return_warning_message_ticket_number_is_invalid_getting_package_failed() throws Exception {
-        String ticketNo = "12345678";
-        String warningMessage = "ticket number is invalid";
-        when(lockerService.getSlotInfoByTicketNoDispatched(ticketNo)).thenReturn(warningMessage);
-        this.mockMvc.perform(get("/slot/12345678")).andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").value(warningMessage));
-    }
-
-    @Test
-    void should_catch_internal_error_during_the_process_of_finding_slot_in_use_and_release_resource() throws Exception {
-        String warningMessage = "Internal Server Error";
-        doThrow(new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, warningMessage))
-                .when(lockerService).getSlotInfoByTicketNoDispatched(anyString());
-        this.mockMvc.perform(get("/slot/12345")).andDo(print())
-                .andExpect(status().isInternalServerError())
-                .andExpect(status().reason(warningMessage))
-                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ResponseStatusException));
+        @Test
+        void should_catch_internal_error_during_the_process_of_finding_slot_in_use_and_release_resource() throws Exception {
+            String warningMessage = "Internal Server Error";
+            doThrow(new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, warningMessage))
+                    .when(lockerService).getSlotInfoByTicketNoDispatched(anyString());
+            this.mockMvc.perform(get("/slot/12345")).andDo(print())
+                    .andExpect(status().isInternalServerError())
+                    .andExpect(status().reason(warningMessage))
+                    .andExpect(result -> assertTrue(result.getResolvedException() instanceof ResponseStatusException));
+        }
     }
 }
--- src/test/java/com/tw/Services/LockerServiceTest.java
+++ src/test/java/com/tw/Services/LockerServiceTest.java
@@ -7,6 +7,7 @@ import com.tw.Repositories.LockerRepository;
 import com.tw.Repositories.SlotRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -57,112 +58,111 @@ public class LockerServiceTest {
     }
 
 
-    /*** Test case: locker 有可用的 slot
-     设计：新方法 lockerRepository.countByHasBagFalseAndLockerId(): LockerStatus {hasAvailableSlot: boolean}
-     **Given**：lockerService.getLockerStatus，通过lockerId，向lockerRepository获取locker status
-     **When**：lockerController 通过lockerService.getLockerStatus 获取locker状态
-     **Then**： lockerService.getLockerStatus应该返回Object LockerStatus {hasAvailableSlot: true}
-     */
+    @Nested
+    class CheckingWhetherThereIsAvailableSlot {
+        @Test
+        void should_return_hasAvailableSlot_as_true_given_locker_has_available_slot() {
+            when(slotRepository.countByHasBagFalseAndLockerId(any(Integer.class))).thenReturn(10L);
 
-    @Test
-    void should_return_hasAvailableSlot_as_true_given_locker_has_available_slot() {
-        when(slotRepository.countByHasBagFalseAndLockerId(any(Integer.class))).thenReturn(10L);
+            LockerStatus lockerStatus = lockerService.getLockerStatus();
 
-        LockerStatus lockerStatus = lockerService.getLockerStatus();
+            verify(slotRepository, times(1)).countByHasBagFalseAndLockerId(integerArgumentCaptor.capture());
+            Integer id = integerArgumentCaptor.getValue();
+            assertThat(id).isEqualTo(locker.getId());
+            assertThat(lockerStatus.isHasAvailableSlot()).isEqualTo(true);
+        }
 
-        verify(slotRepository, times(1)).countByHasBagFalseAndLockerId(integerArgumentCaptor.capture());
-        Integer id = integerArgumentCaptor.getValue();
-        assertThat(id).isEqualTo(locker.getId());
-        assertThat(lockerStatus.isHasAvailableSlot()).isEqualTo(true);
+
+        @Test
+        void should_return_hasAvailableSlot_as_false_given_locker_has_no_available_slot() {
+            when(slotRepository.countByHasBagFalseAndLockerId(any())).thenReturn(0L);
+
+            LockerStatus lockerStatus = lockerService.getLockerStatus();
+            assertThat(lockerStatus.isHasAvailableSlot()).isEqualTo(false);
+        }
     }
 
-    /*** Test case: locker 没有可用的 slot
-     **Given**：lockerService.getLockerStatus，通过lockerId，向lockerRepository获取locker status
-     **When**：lockerController 通过lockerService.getLockerStatus 获取locker状态
-     **Then**： lockerService.getLockerStatus应该返回Object LockerStatus {hasAvailableSlot: false}
-     */
+    @Nested
+    class GettingAvailableSlotToSavePackage {
+        //TODO: locker 中存在可使用的slot的时候，匹配到可使用的slot，返回8位随机数字返回 作为ticket number
+        @Test
+        void should_find_available_slot_and_return_8_bit_random_number_as_ticket_number() {
+            List<Slot> availableSlots = new ArrayList<>(List.of(new Slot(null, 1, false)));
+            Slot savedSlot = new Slot(1, 1, true, "12345678");
+            List<Slot> spyAvailableSlots = spy(availableSlots);
+            when(slotRepository.findByHasBagAndLockerId(any(Boolean.class), any(Integer.class))).thenReturn(spyAvailableSlots);
 
-    @Test
-    void should_return_hasAvailableSlot_as_false_given_locker_has_no_available_slot() {
-        when(slotRepository.countByHasBagFalseAndLockerId(any())).thenReturn(0L);
+            Slot spyAvailableSlot = spy(availableSlots.get(0));
+            when(spyAvailableSlots.get(0)).thenReturn(spyAvailableSlot);
+            when(spyAvailableSlot.dispatchTicketNumber()).thenReturn("12345678");
 
-        LockerStatus lockerStatus = lockerService.getLockerStatus();
-        assertThat(lockerStatus.isHasAvailableSlot()).isEqualTo(false);
+            when(slotRepository.save(any(Slot.class))).thenReturn(savedSlot);
+
+            String ticketNo = lockerService.getTicketNoBindWithDispatchedSlot();
+
+            verify(slotRepository, times(1))
+                    .findByHasBagAndLockerId(booleanArgumentCaptor.capture(), integerArgumentCaptor.capture());
+
+            Boolean hasBag = booleanArgumentCaptor.getValue();
+            Integer lockerId = integerArgumentCaptor.getValue();
+
+            assertThat(ticketNo).isEqualTo("12345678");
+            assertThat(hasBag).isEqualTo(spyAvailableSlot.getHasBag());
+            assertThat(lockerId).isEqualTo(locker.getId());
+        }
+
+        //TODO: locker中不存在可以使用的slot的时候，返回提示对应的提示信息
+        @Test
+        void should_return_warning_message_to_alert_user_there_is_no_available_slot() {
+            List<Slot> availableSlots = new ArrayList<>(List.of());
+            List<Slot> spyAvailableSlots = spy(availableSlots);
+            when(slotRepository.findByHasBagAndLockerId(any(Boolean.class), any(Integer.class))).thenReturn(spyAvailableSlots);
+
+            String warningMessage = lockerService.getTicketNoBindWithDispatchedSlot();
+
+            assertThat(warningMessage).isEqualTo("no available slot can be dispatched, try it later");
+        }
+
+        //TODO: locker中存在可使用的slot的时候，遇到internal error，返回对应的提示信息
+        @Test
+        @Disabled
+        void should_return_internal_error_message_when_it_occurred_given_there_is_available_slots() {
+        }
     }
 
-    //TODO: locker 中存在可使用的slot的时候，匹配到可使用的slot，返回8位随机数字返回 作为ticket number
-    @Test
-    void should_find_available_slot_and_return_8_bit_random_number_as_ticket_number() {
-        List<Slot> availableSlots = new ArrayList<>(List.of(new Slot(null, 1, false)));
-        Slot savedSlot = new Slot(1, 1, true, "12345678");
-        List<Slot> spyAvailableSlots = spy(availableSlots);
-        when(slotRepository.findByHasBagAndLockerId(any(Boolean.class), any(Integer.class))).thenReturn(spyAvailableSlots);
+    @Nested
+    class GettingOccupiedSlotBeforeToTakePackageBack {
+        //TODO: should_return_slot_info_which_has_saved_packages_when_received_dispatched_ticket_number
+        @Test
+        void should_return_occupied_slot_info_which_has_saved_packages_when_received_dispatched_ticket_number() {
+            String ticketNo = "12345678";
+            Optional<Slot> slotOptional = Optional.of(new Slot(123, 1, true, ticketNo));
+            Integer lockerId = locker.getId();
 
-        Slot spyAvailableSlot = spy(availableSlots.get(0));
-        when(spyAvailableSlots.get(0)).thenReturn(spyAvailableSlot);
-        when(spyAvailableSlot.dispatchTicketNumber()).thenReturn("12345678");
+            when(slotRepository.findByHasBagAndTicketNoAndLockerId(eq(Boolean.TRUE), eq(ticketNo), eq(lockerId))).thenReturn(slotOptional);
+            Slot slot = slotOptional.get();
+            slot.releaseSlotResource();
 
-        when(slotRepository.save(any(Slot.class))).thenReturn(savedSlot);
+            String actualResult = lockerService.getSlotInfoByTicketNoDispatched(ticketNo);
+            assertThat(actualResult).isEqualTo(slot.getId().toString());
+        }
 
-        String ticketNo = lockerService.getTicketNoBindWithDispatchedSlot();
+        //TODO: 使用已经使用过的ticket number 取包时，搜索不到对应包的信息，返回不包含任何信息的Slot
+        @Test
+        void should_return_empty_slot_when_given_used_ticket_number() {
+            String ticketNo = "12345678";
+            Integer lockerId = locker.getId();
+            when(slotRepository.findByHasBagAndTicketNoAndLockerId(eq(Boolean.TRUE), eq(ticketNo), eq(lockerId))).thenReturn(Optional.empty());
 
-        verify(slotRepository, times(1))
-                .findByHasBagAndLockerId(booleanArgumentCaptor.capture(), integerArgumentCaptor.capture());
+            String actualResult = lockerService.getSlotInfoByTicketNoDispatched(ticketNo);
 
-        Boolean hasBag = booleanArgumentCaptor.getValue();
-        Integer lockerId = integerArgumentCaptor.getValue();
+            assertThat(actualResult).isEqualTo("ticket number is not valid");
+        }
 
-        assertThat(ticketNo).isEqualTo("12345678");
-        assertThat(hasBag).isEqualTo(spyAvailableSlot.getHasBag());
-        assertThat(lockerId).isEqualTo(locker.getId());
-    }
-
-    //TODO: locker中不存在可以使用的slot的时候，返回提示对应的提示信息
-    @Test
-    void should_return_warning_message_to_alert_user_there_is_no_available_slot() {
-        List<Slot> availableSlots = new ArrayList<>(List.of());
-        List<Slot> spyAvailableSlots = spy(availableSlots);
-        when(slotRepository.findByHasBagAndLockerId(any(Boolean.class), any(Integer.class))).thenReturn(spyAvailableSlots);
-
-        String warningMessage = lockerService.getTicketNoBindWithDispatchedSlot();
-
-        assertThat(warningMessage).isEqualTo("no available slot can be dispatched, try it later");
-    }
-
-    //TODO: locker中存在可使用的slot的时候，遇到internal error，返回对应的提示信息
-
-    @Test
-    @Disabled
-    void should_return_internal_error_message_when_it_occurred_given_there_is_available_slots() {
-
-    }
-
-    //TODO: should_return_slot_info_which_has_saved_packages_when_received_dispatched_ticket_number
-
-    @Test
-    void should_return_occupied_slot_info_which_has_saved_packages_when_received_dispatched_ticket_number() {
-        String ticketNo = "12345678";
-        Optional<Slot> slotOptional = Optional.of(new Slot(123, 1, true, ticketNo));
-        Integer lockerId = locker.getId();
-
-        when(slotRepository.findByHasBagAndTicketNoAndLockerId(eq(Boolean.TRUE), eq(ticketNo), eq(lockerId))).thenReturn(slotOptional);
-        Slot slot = slotOptional.get();
-        slot.releaseSlotResource();
-
-        String actualResult = lockerService.getSlotInfoByTicketNoDispatched(ticketNo);
-        assertThat(actualResult).isEqualTo(slot.getId().toString());
-    }
-
-    //TODO: 使用已经使用过的ticket number 取包时，搜索不到对应包的信息，返回不包含任何信息的Slot
-
-    @Test
-    void should_return_empty_slot_when_given_used_ticket_number() {
-        String ticketNo = "12345678";
-        Integer lockerId = locker.getId();
-        when(slotRepository.findByHasBagAndTicketNoAndLockerId(eq(Boolean.TRUE), eq(ticketNo), eq(lockerId))).thenReturn(Optional.empty());
-
-        String actualResult = lockerService.getSlotInfoByTicketNoDispatched(ticketNo);
-
-        assertThat(actualResult).isEqualTo("ticket number is not valid");
+        //TODO: locker中存在之前已经被占用的slot，在拿ticket number取包的时候 遇到internal error，返回对应的提示信息
+        @Test
+        @Disabled
+        void should_return_internal_error_message_when_it_occurred_given_there_is_available_slots() {
+        }
     }
 }
